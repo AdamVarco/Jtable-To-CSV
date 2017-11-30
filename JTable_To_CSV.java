d67aea083b1e58de21560bbc54c774b970615aaf
Adam Varco <adam.varco@optum.com>
2017-11-30 13:32:37 -0500

    files

---
+++ JTable_To_CSV.java
@@ -0,0 +1,314 @@
+package parse;
+import java.awt.Desktop;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.JButton;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.GroupLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ * @author Adam Mitchell Varco
+ */
+public class JTable_To_CSV extends javax.swing.JFrame {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 3394160863695874427L;
+	/**
+     * Creates new form Java_JTable_Update_Selected_Row_Using_TextBoxes
+     */
+    public JTable_To_CSV() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                         
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextFieldID = new javax.swing.JTextField();
+        jTextFieldFN = new javax.swing.JTextField();
+        jTextFieldLN = new javax.swing.JTextField();
+        jTextFieldAGE = new javax.swing.JTextField();
+        btnUpdateRow = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Verdana", 1, 10)); // NOI18N
+        jLabel1.setText("Id :");
+
+        jLabel2.setFont(new java.awt.Font("Verdana", 1, 10)); // NOI18N
+        jLabel2.setText("First Name :");
+
+        jLabel3.setFont(new java.awt.Font("Verdana", 1, 10)); // NOI18N
+        jLabel3.setText("Last Name :");
+
+        jLabel4.setFont(new java.awt.Font("Verdana", 1, 10)); // NOI18N
+        jLabel4.setText("Age :");
+
+        btnUpdateRow.setText("Update Row");
+        btnUpdateRow.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnUpdateRowActionPerformed(evt);
+            }
+        });
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        		new Object [][] {
+                {"1", "DDFFD", "DGHYTGD", "344"},
+                {"2", "FGHTYH", "UYUYHF", "33"},
+                {"3", "JHGF", "JHGFD", "254"},
+                {"4", "WQER", "OUYREE", "38"},
+                {"5", "BSGH", "IYDGH", "22"},
+                {"6", "QSDTYKJH", "PIHGDX", "16"},
+                {"7", "DSZDD", "UTRFRT", "2567"},
+                {"8", "XCVBGR", "YTEUI", "56"},
+                {"9", "FGHJUY", "NGFDBNJ", "65"},
+                {"10", "WQAZR", "VXWG", "42"}
+            	},
+            new String [] {
+                "Id", "First Name", "Last Name", "Age"
+            }
+        ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTable1);
+        
+        JButton btnNewButton = new JButton("Print to CSV");
+        btnNewButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e){
+        		try {
+        			 printCSV(jTable1.getModel());
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+        	}
+        });
+        
+        btnNewButton_1 = new JButton("Refresh Table from CSV");
+        btnNewButton_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		readCSV(jTable1.getModel());
+        	}
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        layout.setHorizontalGroup(
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addGroup(layout.createParallelGroup(Alignment.LEADING, false)
+        				.addGroup(layout.createSequentialGroup()
+        					.addContainerGap()
+        					.addGroup(layout.createParallelGroup(Alignment.TRAILING)
+        						.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        							.addGroup(layout.createSequentialGroup()
+        								.addGroup(layout.createParallelGroup(Alignment.TRAILING)
+        									.addComponent(jLabel3)
+        									.addComponent(jLabel4))
+        								.addPreferredGap(ComponentPlacement.UNRELATED)
+        								.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        									.addComponent(jTextFieldAGE, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+        									.addComponent(jTextFieldLN, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)))
+        							.addGroup(layout.createSequentialGroup()
+        								.addGroup(layout.createParallelGroup(Alignment.TRAILING)
+        									.addComponent(jLabel1)
+        									.addComponent(jLabel2))
+        								.addPreferredGap(ComponentPlacement.RELATED)
+        								.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        									.addComponent(jTextFieldFN, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)
+        									.addComponent(jTextFieldID, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE))))
+        						.addGroup(layout.createParallelGroup(Alignment.LEADING, false)
+        							.addComponent(btnNewButton, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        							.addComponent(btnUpdateRow, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)))
+        					.addGap(18))
+        				.addGroup(Alignment.TRAILING, layout.createSequentialGroup()
+        					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        					.addComponent(btnNewButton_1)
+        					.addGap(57)))
+        			.addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 452, GroupLayout.PREFERRED_SIZE)
+        			.addContainerGap(30, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addGroup(layout.createParallelGroup(Alignment.LEADING)
+        				.addGroup(layout.createSequentialGroup()
+        					.addGap(12)
+        					.addComponent(btnNewButton_1)
+        					.addPreferredGap(ComponentPlacement.UNRELATED)
+        					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        						.addComponent(jLabel1, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+        						.addComponent(jTextFieldID, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        					.addGap(18)
+        					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        						.addComponent(jLabel2, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+        						.addComponent(jTextFieldFN, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        					.addGap(18)
+        					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        						.addComponent(jLabel3, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+        						.addComponent(jTextFieldLN, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        					.addGap(18)
+        					.addGroup(layout.createParallelGroup(Alignment.BASELINE)
+        						.addComponent(jLabel4, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+        						.addComponent(jTextFieldAGE, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        					.addGap(28)
+        					.addComponent(btnUpdateRow, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+        					.addGap(18)
+        					.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE))
+        				.addGroup(layout.createSequentialGroup()
+        					.addGap(32)
+        					.addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 260, GroupLayout.PREFERRED_SIZE)))
+        			.addContainerGap())
+        );
+        getContentPane().setLayout(layout);
+
+        pack();
+    }// </editor-fold>                       
+
+    private void btnUpdateRowActionPerformed(java.awt.event.ActionEvent evt) {                                           
+
+        int i = jTable1.getSelectedRow();
+         DefaultTableModel model = (DefaultTableModel)jTable1.getModel();
+        if(i >= 0)
+        {
+            model.setValueAt(jTextFieldID.getText(), i, 0);
+            model.setValueAt(jTextFieldFN.getText(), i, 1);
+            model.setValueAt(jTextFieldLN.getText(), i, 2);
+            model.setValueAt(jTextFieldAGE.getText(), i, 3);
+        }else{
+            JOptionPane.showMessageDialog(null, "Error");
+        }
+      
+    }                                           
+
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {                                   
+    
+        int selectedRow = jTable1.getSelectedRow();
+        DefaultTableModel model = (DefaultTableModel)jTable1.getModel();
+        jTextFieldID.setText(model.getValueAt(selectedRow, 0).toString());
+        jTextFieldFN.setText(model.getValueAt(selectedRow, 1).toString());
+        jTextFieldLN.setText(model.getValueAt(selectedRow, 2).toString());
+        jTextFieldAGE.setText(model.getValueAt(selectedRow, 3).toString());
+    }                                   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JTable_To_CSV.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JTable_To_CSV.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JTable_To_CSV.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JTable_To_CSV.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new JTable_To_CSV().setVisible(true);
+            }
+        });
+    }
+    void printCSV(TableModel tableModel) throws Exception{
+    	StringBuilder stB = new StringBuilder();
+
+		PrintWriter pw = new PrintWriter(new File(filepath));
+		List<String> numdata = new ArrayList<String>();
+		 for (int count = 0; count < tableModel.getRowCount(); count++){
+             numdata.add(tableModel.getValueAt(count, 0).toString());
+             numdata.add(",");
+             numdata.add(tableModel.getValueAt(count, 1).toString());
+             numdata.add(",");
+             numdata.add(tableModel.getValueAt(count, 2).toString());
+             numdata.add(",");
+             numdata.add(tableModel.getValueAt(count, 3).toString());
+             numdata.add("\n");
+       }
+		 for(String s:numdata) {
+			 stB.append(s);
+		 }
+		pw.write(stB.toString());
+		pw.close();
+		
+		Desktop desktop = Desktop.getDesktop();
+		File file = new File(filepath);
+		if(file.exists() && file.canRead()) 
+		if(file.exists() && file.isFile()) 
+        	desktop.open(file);
+		else 
+			throw new FileNotFoundException();
+		
+    }
+    void readCSV(TableModel model) {
+    	 BufferedReader br = null;
+         String line = "";
+         try {
+        	 int row =0;
+            br = new BufferedReader(new FileReader(filepath));
+             while ((line = br.readLine()) != null) {
+               String[] table = line.split(",");
+               jTextFieldID.setText(table[0]);
+               jTextFieldFN.setText(table[1]);
+               jTextFieldLN.setText(table[2]);
+               jTextFieldAGE.setText(table[3]);
+               model.setValueAt(jTextFieldID.getText(), row, 0);
+               model.setValueAt(jTextFieldFN.getText(), row, 1);
+               model.setValueAt(jTextFieldLN.getText(), row, 2);
+               model.setValueAt(jTextFieldAGE.getText(), row, 3);
+               row++;
+                }
+             
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+		e.printStackTrace();
+	  }
+    }
+    
+    String filepath = System.getProperty("user.home") + "\\" + "Desktop\\MyCSV.csv";
+    private javax.swing.JButton btnUpdateRow;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField jTextFieldAGE;
+    private javax.swing.JTextField jTextFieldFN;
+    private javax.swing.JTextField jTextFieldID;
+    private javax.swing.JTextField jTextFieldLN;
+    private JButton btnNewButton_1;
+}
